5e408b3cd1007be7a776c082c8fa51e933192a2a
Roman Kotchetov <Roman.Kotchetov@gmail.com>
2020-11-14 10:33:20 -0800

    Uhh yeah

--- src/App.js
+++ src/App.js
@@ -1,48 +1,68 @@
-import React, { Fragment } from 'react';
-import './StyleSheets/App.css';
-
-// import ClassForm from './Components/Forms/classForm'
-// import HookForm from './Components/Forms/hookForm';
-// import RefactoredHookForm from './Components/Forms/refactoredHookForm'
-// import AbstractHookForm from './Components/Forms/abstractHookForm'
-// import CalendarForm from './Components/Forms/calendarForm'
-
-// import ClassCounter from './Components/Counters/classCounter'
-// import HookCounter from './Components/Counters/hookCounter'
-// import HookReducerCounter from './Components/Counters/hookCounterWithReducer'
-
-// import ToDoHook from './Components/ToDos/toDoHooks'
-
-// import FunctionDailyPic from './Components/DataFetch/functionFetchData'
-// import ClassDailyPic from './Components/DataFetch/classFetchData'
-
-// import DetailsModal from './Components/DataFetch/modal'
-
-import Search from './Components/Search/search'
-
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Todo from './components/Todo';
+import TodoForm from './components/Form';
+// import List from './components/List';
 
 function App() {
-	return (
-		<Fragment>
-			<div> 
-				<Search/>
-				{/* <CalendarForm/> */}
-				{/* <DetailsModal /> */}
-				{/* <FunctionDailyPic /> */}
-				{/* <ClassDailyPic /> */}
-				{/* <ToDoHook/> */}
-				{/* <HookReducerCounter/> */}
-				{/* <ClassCounter/>
-				<HookCounter/> */}
-				{/* <ClassForm/>
-				<HookForm/> 
-				<RefactoredHookForm/>
-				<AbstractHookForm/> */}
-			</div>
-		</Fragment>
-	);
+    const [todos, setTodos] = useState([]);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        console.log('loaded todos');
+        fetch('https://jsonplaceholder.typicode.com/users/1/todos')
+            .then((response) => response.json())
+            .then((json) => setTodos(json))
+    }, []);
+
+    useEffect(() => {
+        console.log('resized');
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    }
+
+    const addTodo = title => {
+        console.log(title);
+        const newTodos = [...todos, { title }];
+        setTodos(newTodos);
+    }
+
+    const completeTodo = index => {
+        console.log(index);
+        const newTodos = [...todos];
+        newTodos[index].complete = (!newTodos[index].complete);
+        setTodos(newTodos);
+    }
+
+    const removeTodo = index => {
+        console.log(index);
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    }
+
+    return (
+        <div className="app">
+            {windowWidth}
+            <div className="todo-list">
+                {todos.map((todo, index) => (
+                    <Todo
+                        key={index}
+                        index={index}
+                        todo={todo}
+                        completeTodo={completeTodo}
+                        removeTodo={removeTodo} />
+                ))}
+                <TodoForm addTodo={addTodo} />
+            </div>
+        </div>
+    )
 }
 
 export default App;
